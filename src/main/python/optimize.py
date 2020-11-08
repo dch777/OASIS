bbb4d114ddacb7eb6a8d3952b6838777ff455c72
dch777 <d.chanana777@gmail.com>
2020-11-08 10:58:48 -0600

    Added output variables

--- src/main/python/optimize.py
+++ src/main/python/optimize.py
@@ -1,5 +1,6 @@
 import numpy as np
 from xfoil import XFoil
+import airfoils
 
 nacaSet = [6, 7, 9, 10, 12, 15, 18, 21, 24, 1408, 1410, 1412, 2408, 2410, 2513, 6409,
         2411, 2412, 2414, 2415, 2418, 2421, 2525, 4412, 4415, 4418, 4421, 4424, 6412]
@@ -17,8 +18,11 @@ def getCoefficients(naca, reynolds=1e6, iterations=20, angle=10, angle_step=.5):
 def getBestAirfoil(reynolds=1e6, iterations=20, angle=10):
     liftDragRatio = np.array([getCoefficients(naca, reynolds=reynolds, iterations=iterations, angle=angle)[0] for naca in nacaSet])
     max_idx = np.argmax(liftDragRatio.T[0])
-    return nacaSet[max_idx], liftDragRatio[max_idx][1]
+    foil = airfoils.airfoils.Airfoil.NACA4(f"{nacaSet[max_idx]:04d}")
+    foil.plot()
+    return nacaSet[max_idx], liftDragRatio[max_idx]
 
-def graphAirfoilEfficiency(naca, reynolds=1e6, iterations=20, angle=10, angle_step=.5)
+def graphAirfoilEfficiency(naca, reynolds=1e6, iterations=20, angle=10, angle_step=.5):
+    pass
 
 print(getBestAirfoil(reynolds=1e8, angle=20))
